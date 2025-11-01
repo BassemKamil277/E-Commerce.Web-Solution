12d7ca09201a7459e20a6362621c2af6fc109bfd
Bassem <Bassem@DESKTOP-MDUDI8C>
2025-11-01 17:38:24 +0200

    Add project files.

---
+++ E Commerce.Domain/Entites/ProductModule/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entites.ProductModule
+{
+    public class Product : BaseEntity<int>
+    {
+ 
+        public string Name { get; set; } = default!;
+
+        public string Description { get; set; } = default!;
+
+        public string PictureUrl { get; set; } = default!;
+        public decimal Price { get; set; }
+
+
+        #region RelationShips
+
+        #region product - ProductBrand
+        public int BrandId { get; set; }
+        public ProductBrand ProductBrand { get; set; } = default!;
+        #endregion
+
+
+        #region product - ProductType
+        public int TypeId { get; set; }
+        public ProductType ProductType { get; set; } = default!;
+        #endregion
+
+
+        #endregion
+    }
+}
---
+++ E Commerce.Persistence/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using E_Commerce.Domain.Entites.ProductModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name)
+                   .HasMaxLength(100);
+
+
+            builder.Property(x => x.Description)
+                 .HasMaxLength(500);
+
+
+            builder.Property(x => x.PictureUrl)
+                 .HasMaxLength(200);
+
+
+
+            builder.Property(x => x.Price)
+                 .HasPrecision(18,2);
+
+            builder.HasOne(x => x.ProductBrand)
+                   .WithMany() // Product 
+                   .HasForeignKey(x => x.BrandId);
+
+            builder.HasOne(x => x.ProductType)
+            .WithMany() // Product 
+            .HasForeignKey(x => x.TypeId);
+        }
+    }
+}
